39f1aae05a44d51fa619942a131893e25b28fdde
Cristina <16151342@aguascalientes.tecnm.mx>
2020-06-04 16:45:22 -0500

    Actualizacion

---
+++ src/data.js
@@ -0,0 +1,113 @@
+
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Exfoliante de fresa",
+    img: "img/producto1.jpg",
+    price: 349,
+    company: "fresh",
+    info:
+      "Un limpiador facial exfoliante suave en un tamaño de 200 ml que elimina las impurezas y el aceite, sin resecarlo, mientras refina el aspecto de los poros para una piel suave y tersa.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Serum de Rosa",
+    img: "img/producto2.jpg",
+    price: 499,
+    company: "fresh",
+    info:
+      "Un suero ligero y de rápida absorción que proporciona una humedad profunda de 24 horas para una piel flexible y húmeda",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Crema de té negro",
+    img: "img/producto3.jpg",
+    price: 299,
+    company: "fresh",
+    info:
+      "Una crema para la cara y el cuello visiblemente reafirmante que hidrata y fortalece la piel para una apariencia más firme y alzada con contornos más definidos.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Gel de rosa",
+    img: "img/producto4.jpg",
+    price: 249,
+    company: "fresh",
+    info:
+      "Una crema hidratante diaria sin peso para todo tipo de piel (¡incluso grasa!) Que proporciona hidratación las 24 horas con un acabado mate fácil de maquillar.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Gel de ojos de loto",
+    img: "img/producto5.jpg",
+    price: 299,
+    company: "fresh",
+    info:
+      "Un gel para los ojos ligero y diario que reduce la apariencia de hinchazón, ojeras y fatiga mientras hidrata y revitaliza",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Tónico de arcilla",
+    img: "img/producto6.jpg",
+    price: 199,
+    company: "fresh",
+    info:
+      "Un tóner de control de aceite a base de arcilla que minimiza la apariencia de los poros mientras matifica y suaviza la piel.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Suero Antioxidante",
+    img: "img/producto7.jpg",
+    price: 449,
+    company: "fresh",
+    info:
+        "Un suero de jugo con más de 480,0000 microburbujas ricas en nutrientes que brinda una dosis esencial de nutrición para la piel para una apariencia energizada y saludable",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Crema de Lotus",
+    img: "img/producto8.jpg",
+    price: 499.00,
+    company: "fresh",
+    info:
+      "Una crema de recuperación nocturna de vanguardia impulsada por el antioxidante Super Lotus que ayuda a fortalecer la barrera cutánea mientras combate la opacidad y la deshidratación.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Lavado facial exfoliante de fresa y azúcar",
+  img: "img/producto1.jpg",
+  price: 349,
+  company: "fresh",
+  info:
+      "Un limpiador facial exfoliante suave en un tamaño de 200 ml que elimina las impurezas y el aceite, sin resecarlo, mientras refina el aspecto de los poros para una piel suave y tersa.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
