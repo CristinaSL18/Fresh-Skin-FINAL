e62b78422b1d7a0162e66d495b69a7073007e0e8
Cristina <16151342@aguascalientes.tecnm.mx>
2020-06-10 22:08:29 -0500

    Actualizacion

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+    class Footer extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <footer className="footer page-footer font-small special-color-dark pt-4">
+                    <div className="footer-copyright text-center py-3">© 2020 Copyright:
+                        <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+                    </div>
+                </footer>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Footer;
---
+++ src/components/Inicio.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Title from "./Title";
+import Slides from "./Slides";
+import BannerTop from "./BannerTop";
+import Gallery from "./Gallery";
+
+class Inicio extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Title name="Bienvenidos" />
+                <Slides/>
+                <BannerTop/>
+                <Title name="Conocenos" />
+                <Gallery/>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Inicio;
